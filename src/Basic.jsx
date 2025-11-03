27b3114f3e1105378ccbc1df258ccc0e4230faff
AkashKakkat <akashkakkat001@gmail.com>
2025-11-03 11:35:22 +0400

    react commit

---
+++ src/Basic.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Basic = () => {
+  return (
+    <>
+        <h2>Hellooo...</h2>
+        <img src="" alt="" />
+    </>
+  )
+}
+
+export default Basic
---
+++ src/embedding-expression/Welcome.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Welcome = () => {
+    let name = 'akash';
+  return (
+    <div>
+      <h2>Hi  {name}</h2>
+    </div>
+  )
+}
+
+export default Welcome
